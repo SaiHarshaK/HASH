aad07b4d2a6edca2c842dfc7f8e7dd8fe1295c79
Sagar Jain <j.sagar700@gmail.com>
2019-04-24 13:52:09 +0530

    Added Tests for parser

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,20 @@
+module Main where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+import Helpers
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  defaultMain (testGroup "Our Library Tests" [parseCommandTest])
+
+parseCommandTest :: TestTree
+parseCommandTest = testGroup "Testing parseCommand"
+	[
+		testCase "Testing parseCommand Single Argument"
+  		(assertEqual "Should split into command and arguments" ("nano", "helloworld.txt") (parseCommand "nano helloworld.txt")),
+		testCase "Testing parseCommand Multiple Arguments"
+  		(assertEqual "Should split into command and arguments" ("cp", "Documents/Book.pdf Downloads/Books/") (parseCommand "cp Documents/Book.pdf Downloads/Books/")),
+		testCase "Testing parseCommand with Flags"
+  		(assertEqual "Should split into command and arguments" ("ls", "-la") (parseCommand "ls -la"))
+	]
