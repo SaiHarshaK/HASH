bc3c4a5591bd00ba73664ca4ff832af14fcef03e
SaiHarshaK <k.saiharsha7@gmail.com>
2019-04-24 16:28:55 +0530

    ctrlC handle

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,9 @@ import System.Process
 import System.Directory
 import System.Posix.User
 import System.IO
+import System.Exit
+import System.Posix.Signals
+import Control.Concurrent
 import Helpers
 
 builtins :: [String]
@@ -10,12 +13,14 @@ builtins = ["cd"]
 -- The command prompter for the shell
 prompt :: IO ()
 prompt = do
-	-- get the username of the user
+  -- get the username of the user
         userName <- getEffectiveUserName
-       	-- print the prompt
+        -- print the prompt
         putStr (userName ++ "@hash> ")
         -- flush stdout
         hFlush stdout
+        -- In case of interrupts, handle them instead of exiting shell
+        installHandler keyboardSignal (Catch ctrlC) Nothing
         command <- getLine
         -- Handle the incoming command
         handleCommand command
@@ -24,20 +29,24 @@ prompt = do
 handleCommand :: String -> IO()
 -- if exit is typed, we return
 handleCommand command = if command == "exit" then do
-		                	putStrLn "Bye :)"
-		                	return ()
-		                else do 
-		                	-- execute the line of command
-		                    executeLine command
-		                    prompt
+                      putStrLn "Bye :)"
+                      return ()
+                        else do
+                      -- execute the line of command
+                        executeLine command
+                        prompt
 
 main :: IO ()
 main =  prompt
 
 executeLine :: String -> IO ()
 executeLine [] = putStr ""
-executeLine a = do 
+executeLine a = do
                 system a
                 return ()
 
-
+ctrlC :: IO ()
+ctrlC = do
+    putStrLn ""
+    putStrLn "interrupt"
+    prompt
